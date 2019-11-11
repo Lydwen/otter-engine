085080a061c10affafbc173c599b756764e8d958
Tom Dall'Agnol <tomlau94@aol.com>
2019-11-11 13:00:58 +0100

    Add an overload on the + operator for vector addition

--- OtterEngine/MathEngine/include/Vector3.hpp
+++ OtterEngine/MathEngine/include/Vector3.hpp
@@ -3,15 +3,24 @@
 class Vector3 {
 private:
 public:
+  double x;
+  double y;
+  double z;
+
   Vector3(double x1, double y1, double z1) {
     x = x1;
     y = y1;
     z = z1;
   }
 
-  double x;
-  double y;
-  double z;
+  Vector3 operator+(Vector3 other) {
+    double newX = x + other.x;
+    double newY = y + other.y;
+    double newZ = z + other.z;
+
+    Vector3 newVector(newX, newY, newZ);
+    return newVector;
+  }
 
   Vector3 Vector3::Add(Vector3 other);
 };
--- OtterEngine/MathEngine/tests/VectorTest.cpp
+++ OtterEngine/MathEngine/tests/VectorTest.cpp
@@ -9,5 +9,14 @@ TEST(VectorTest, TestAddTwoVectorsWillCorrectlyMakeTheAdditionOfTheirMembers) {
   EXPECT_EQ(11, result.x);
   EXPECT_EQ(22, result.y);
   EXPECT_EQ(33, result.z);
+}
 
+TEST(VectorTest, TestOverloadOperatorPlus) {
+  Vector3 vectorLeft(1, 2, 3);
+  Vector3 vectorRight(10, 20, 30);
+
+  Vector3 result = vectorLeft + vectorRight;
+  EXPECT_EQ(11, result.x);
+  EXPECT_EQ(22, result.y);
+  EXPECT_EQ(33, result.z);
 }
